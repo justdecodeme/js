3045f4e684b0f1e52d5fe4b22bfcd2d12e8aef2d
Rakesh <justdecodeme@hotmail.com>
2017-12-15 14:29:19 +0530

    book title changed

--- notes/Learning Javascript Data Structure and Algorithm - Loiane Groner.js
+++ notes/Learning Javascript Data Structure and Algorithm - Loiane Groner.js
@@ -1,6 +1,6 @@
-/****************************************************
-          YDKJS - UP & GOING - KYLE SIMPSON
-****************************************************/
+/***********************************************************************************
+          Learning Javascript Data Structure and Algorithm - Loiane Groner          
+***********************************************************************************/
 
 /***********/
 /* Preface */
@@ -8,9 +8,9 @@
 // JS has grown so much that is no longer just a frontend language; 
 // it is also present now on the server (NodeJS) and database as well (MongoDB)
 
-/**************/
-/* Chapter 01 */
-/**************/
+/**********************************************/
+/* Chapter 01 - JavaScript - A Quick Overview */
+/**********************************************/
 // Learning data structures and algorithms is very important for any technology professional. Since
 // Data structure and algorithms can solve the most common problems efficiently. This will make 
 // a difference to the quality of the source code you write in the future (including performance)
@@ -18,3 +18,7 @@
 // As best practice, we should include any js code at the end of the body tag. This way, the HTML will 
 // be parsed by the browser and displayed before the scripts are loaded. This boosts the performance 
 // of the page.
+
+// You may hear that global variables in JS are evil and this is true. Usually, the quality of JS source code
+// is measured by the number of global variables and functions (a large number is bad). So, whenever possible,
+// try avoiding global variables.
