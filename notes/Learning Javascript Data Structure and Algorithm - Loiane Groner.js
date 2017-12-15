35812bbd10a08927ac10ffe56f82aa9242e57153
Rakesh <justdecodeme@hotmail.com>
2017-12-15 14:23:46 +0530

    Writing Notes

--- notes/Learning Javascript Data Structure and Algorithm - Loiane Groner.js
+++ notes/Learning Javascript Data Structure and Algorithm - Loiane Groner.js
@@ -2,37 +2,19 @@
           YDKJS - UP & GOING - KYLE SIMPSON
 ****************************************************/
 
-/************/
-/* PAGE VII */
-/************/
-// Brendan Eich ?
-// Brendan Eich (born July 4, 1961) is an American technologist and creator of the JavaScript programming language.
-// He co-founded the Mozilla project, the Mozilla Foundation and the Mozilla Corporation, and served as the Mozilla Corporation's
-// chief technical officer and briefly its chief executive officer. He is the CEO of Brave Software.
-
-// "Javascript" is related to "Java" as "Carnival" is to "Car"
-
 /***********/
-/* PAGE 04 */
+/* Preface */
 /***********/
-// It's typically asserted that JS is interpreted, because your JS source code is processed each time it's run. But that's not
-// entirely accurate. The JS engine actually compiles the program on the fly and then immediately runs the compiled code.
+// JS has grown so much that is no longer just a frontend language; 
+// it is also present now on the server (NodeJS) and database as well (MongoDB)
 
-/***********/
-/* PAGE 11 */
-/***********/
-// Coercion: The conversion of value from one type to another in JS is called coercion. There are two types
-// 1. explicit coercion
-	var a = "4";
-	var b = Number(a);
-	console.log(a);  // "4"
-	console.log(b);  // 4
-	typeof a;  // string
-	typeof b;  // number
-// 2. implicit coercion
-	"4" == 4;  // true, due to "4" here converts to 4 or vice versa ?
+/**************/
+/* Chapter 01 */
+/**************/
+// Learning data structures and algorithms is very important for any technology professional. Since
+// Data structure and algorithms can solve the most common problems efficiently. This will make 
+// a difference to the quality of the source code you write in the future (including performance)
 
-	var a = "4";
-	var b = "5";
-	b = a * 1;  // "4" implicitly coerced to 4 here
-
+// As best practice, we should include any js code at the end of the body tag. This way, the HTML will 
+// be parsed by the browser and displayed before the scripts are loaded. This boosts the performance 
+// of the page.
