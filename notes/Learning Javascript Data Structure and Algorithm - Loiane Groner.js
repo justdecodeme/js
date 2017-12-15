f5b25f9a5d46aee54d3378626b1407046154b404
Rakesh <justdecodeme@hotmail.com>
2017-12-15 21:44:30 +0530

    classes file added + notes updated

--- notes/Learning Javascript Data Structure and Algorithm - Loiane Groner.js
+++ notes/Learning Javascript Data Structure and Algorithm - Loiane Groner.js
@@ -31,14 +31,25 @@
 // ECMAScript 2015 == ES6
 // ECMAScript 2016 == ES7
 
-// ECMAScript 6 functionalities
-// ----------------------------
+// ECMAScript 6 functionalities (✗✓)
+// ----------------------------------
 // let and const (✓)
 // Template literals (✓)
-// Destructuring Assignment (✗)
+// Destructuring Assignment (✓)
 // Spread and Rest operator (✓)
 // Arrow functions using => (✓)
-// Classes (✗)
+// Classes (✓)
 // Default parameters (✓)
+// iterators (✗)
+// typed arrays (✗)
+// Set  (✗)
+// Map (✗)
+// WeakSet (✗)
+// WeakMap (✗)
+// modules (✗)
+// tail calls (✗)
+// Symbol (✗)
 
 // let and const has same begavior except variable defined as const has a read-only value, meaning a constant value.
+
+// It is good to remember that JavaScript object-oriented programming is done through prototype
