3fcfe6f4d2a5e6e8b76c61d14994353ca6560a52
Rakesh <justdecodeme@hotmail.com>
2019-11-21 16:23:15 +0530

    code refractored

--- projects/audio-visualizer/script.js
+++ projects/audio-visualizer/script.js
@@ -1,49 +1,53 @@
+var file = document.getElementById("thefile");
+var audio = document.getElementById("audio");
+var playPauseBtn = document.getElementById("playPause");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var renderFrame = null;
+var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+                            window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+var cancelAnimationFrame = window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+var myReq;
+
 window.onload = function() {
-  
-  var file = document.getElementById("thefile");
-  var audio = document.getElementById("audio");
-  var playPauseBtn = document.getElementById("playPause");
-  
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+
   file.onchange = function() {
     var files = this.files;
     audio.src = URL.createObjectURL(files[0]);
     audio.load();
-    // audio.play();
+
     var context = new AudioContext();
     var src = context.createMediaElementSource(audio);
     var analyser = context.createAnalyser();
-
-    var canvas = document.getElementById("canvas");
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    var ctx = canvas.getContext("2d");
+    // console.log(src, analyser)
 
     src.connect(analyser);
     analyser.connect(context.destination);
 
     analyser.fftSize = 256;
 
-    var bufferLength = analyser.frequencyBinCount;
-    // console.log(bufferLength);
+    var bufferLength = analyser.frequencyBinCount; // 128
 
     var dataArray = new Uint8Array(bufferLength);
+    // console.log(bufferLength, dataArray)
 
-    var WIDTH = canvas.width;
-    var HEIGHT = canvas.height;
+    var cvW = canvas.width;
+    var cvH = canvas.height;
 
-    var barWidth = (WIDTH / bufferLength) * 2.5;
+    var barWidth = (cvW / bufferLength) * 2.5;
     var barHeight;
     var x = 0;
 
-    function renderFrame() {
-      requestAnimationFrame(renderFrame);
-
+    renderFrame = () => {
+      // console.log('renderFrame');
       x = 0;
 
       analyser.getByteFrequencyData(dataArray);
 
       ctx.fillStyle = "#000";
-      ctx.fillRect(0, 0, WIDTH, HEIGHT);
+      ctx.fillRect(0, 0, cvW, cvH);
 
       for (var i = 0; i < bufferLength; i++) {
         barHeight = dataArray[i];
@@ -53,21 +57,22 @@ window.onload = function() {
         var b = 50;
 
         ctx.fillStyle = "rgb(" + r + "," + g + "," + b + ")";
-        ctx.fillRect(x, HEIGHT - barHeight, barWidth, barHeight);
+        ctx.fillRect(x, cvH - barHeight, barWidth, barHeight);
 
         x += barWidth + 1;
       }
-    }
 
-    // audio.play();
-    renderFrame();
+      myReq = requestAnimationFrame(renderFrame);
+    }
   };
 
   playPauseBtn.addEventListener('click', () => {
     if(audio.paused) {
       audio.play();
+      myReq = requestAnimationFrame(renderFrame);
     } else {
       audio.pause();
+      cancelAnimationFrame(myReq);
     }
   })
 };
